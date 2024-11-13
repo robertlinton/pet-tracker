80d5158de15d1954ce5374610973f16780ea1d4e
Robert Linton <146392195+robertlinton@users.noreply.github.com>
2024-11-13 03:13:00 -0500

    new

--- app/pets/[id]/page.tsx
+++ app/pets/[id]/page.tsx
@@ -1,12 +1,11 @@
-// eslint-disable
-
 // app/pets/[id]/page.tsx
+
 import { Suspense } from 'react';
 import { PetOverviewClient } from './pet-overview-client';
 import { Loading } from '@/components/ui/loading';
 
 interface PetOverviewPageProps {
-  params: { id: string }; // Use a plain object without async Promise type
+  params: { id: string };
 }
 
 export default function PetOverviewPage({ params }: PetOverviewPageProps) {
